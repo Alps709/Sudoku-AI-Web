0ff15552688ce01a86f9a952fc8a6facc6155ffb
Anton Le Prevost-Smith <alps709@hotmail.com>
2020-08-26 05:36:42 +1200

    Fixed the sudoku brute force function

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 ////////////////////
-//Sudoku variables//
+//Sudoku letiables//
 ////////////////////
 
 //Max board size
 let MBS = 9;
 let EMPTY = 0;
 
-var iterations = 0;
+let iterations = 0;
 
 //A 3d array that keeps track of what viable number placement options there are for each position on the board
-var viableNumberChecks = [];
+let viableNumberChecks = [];
+let viableNumberChecksSorted = [];
+
+
 
 //Initialise board
-var board  = 
+let board  = 
 [
     [0, 0, 0,  0, 0, 0,  0, 0, 0],
     [0, 0, 0,  0, 0, 0,  0, 0, 0],
@@ -27,7 +30,7 @@ var board  =
     [0, 0, 0,  0, 0, 0,  0, 0, 0]
 ];
 
-var emptyBoard = 
+let emptyBoard = 
 [
     [0, 0, 0,  0, 0, 0,  0, 0, 0],
     [0, 0, 0,  0, 0, 0,  0, 0, 0],
@@ -42,7 +45,7 @@ var emptyBoard =
     [0, 0, 0,  0, 0, 0,  0, 0, 0]
 ];
 
-var defaultBoard = 
+let defaultBoard = 
 [
     [8, 0, 0,  4, 0, 6,  0, 0, 7],
     [0, 0, 0,  0, 0, 0,  4, 0, 0],
@@ -61,14 +64,14 @@ var defaultBoard =
 //Sudoku Function//
 ///////////////////
 
-function FindEmpty(_board)
+function FindEmpty()
 {
     var returnVals = [];
     for (var i = 0; i < MBS; i++)
     {
         for (var j = 0; j < MBS; j++)
         {
-            if(_board[i][j] == EMPTY)
+            if(board[i][j] == EMPTY)
             {
                 returnVals = [true, i, j];
                 return returnVals;
@@ -79,11 +82,10 @@ function FindEmpty(_board)
     return returnVals;
 }
 
-
 //Checks if the _x row is clear of the _numToCheck
 function RowClear(_board, _x, _numToCheck)
 {
-    for (var i = 0; i < MBS; i++)
+    for (let i = 0; i < MBS; i++)
     {
         if(_board[_x][i] == _numToCheck)
         {
@@ -96,7 +98,7 @@ function RowClear(_board, _x, _numToCheck)
 //Checks if the _y column is clear of the _numToCheck
 function ColumnClear(_board, _y, _numToCheck)
 {
-    for (var i = 0; i < MBS; i++)
+    for (let i = 0; i < MBS; i++)
     {
         if(_board[i][_y] == _numToCheck)
         {
@@ -112,9 +114,9 @@ function BoxClear(_board, _x, _y, _numToCheck)
     let xBoxStart = _x - (_x % 3);
     let yBoxStart = _y - (_y % 3);
     
-    for (var i = 0; i < 3; i++)
+    for (let i = 0; i < 3; i++)
     {
-        for (var j = 0; j < 3; j++)
+        for (let j = 0; j < 3; j++)
         {
             if(_board[i + xBoxStart][j + yBoxStart] == _numToCheck){ return false; }
         }
@@ -131,35 +133,50 @@ function CanPlace(_board, _x, _y, _numToCheck)
 }
 
 //Finds all the numbers that are possibly viable to place in a tile
-function FindViableChecks(_board)
+function FindViableChecks()
 {
     //Reset the 3D array
     viableNumberChecks = viableNumberChecks.splice(0, viableNumberChecks.length);
     viableNumberChecks = CreateArray(MBS, MBS, 0);
-    console.log(viableNumberChecks);
 
-    for (var x = 0; x < MBS; x++)
+    for (let x = 0; x < MBS; x++)
     {
-        for (var y = 0; y < MBS; y++)
+        for (let y = 0; y < MBS; y++)
         {
             //Check if we can even place something
-            if(_board[x][y] == EMPTY)
+            if(board[x][y] == EMPTY)
             {
-                for (var num = 1; num <= MBS; num++)
+                for (let num = 1; num <= MBS; num++)
                 {
-                    if(CanPlace(_board, x, y, num))
+                    if(CanPlace(board, x, y, num))
                     {
                         viableNumberChecks[x][y].push(num);
                     }
                 }
             }
+            let PositionNum = 
+            {
+                pos: [x, y],
+                arrayLength: viableNumberChecks[x][y].length
+            }
+
+            viableNumberChecksSorted.push(PositionNum);
         }
     }
 
+    console.log(viableNumberChecks);
+
+    viableNumberChecksSorted.sort(function(a, b)
+    {
+        return a.arrayLength - b.arrayLength;
+    });
+
+    console.log(viableNumberChecksSorted);
+
     //Shuffle the sequence of random numbers so that it has a better chance of quickly beating backtracking-counter-boards (the difficult board below was specifically designed to make it take long for backtracking to complete, shuffling the board this way makes it so a board can't be designed to make it take as long as possible, although this does mean the algorithmn will result in different solve times, even on the same board)
-    // for (var x = 0; x < MBS; x++)
+    // for (let x = 0; x < MBS; x++)
     // {
-    //     for (var y = 0; y < MBS; y++)
+    //     for (let y = 0; y < MBS; y++)
     //     {
     //         //If there is only 1 viable number for a position, then place that number
     //         if(viableNumberChecks[x][y].count == 1)
@@ -177,10 +194,12 @@ function FindViableChecks(_board)
     return viableNumberChecks;
 }
 
+let counter = 0;
 //Sudoku recursive brute force back-tracking function
-function BruteForceSudoku(_board)
+function BruteForceSudoku()
 {
-    var foundEmptySlot = FindEmpty(_board);
+    console.log(counter++);
+    var foundEmptySlot = FindEmpty(board);
     //Find the next empty position on the board
     if(!foundEmptySlot[0])
     {
@@ -193,30 +212,22 @@ function BruteForceSudoku(_board)
     //Only check the numbers that aren't already in this row, column or box
     for(var i = 0; i < viableNumberChecks[x][y].length; i++)
     {
-        iterations += 1;
-
-        //Print a counter every 100,000 iterations
-        if(iterations % 1000 == 0)
-        {
-            console.log("Current interations:", iterations);
-        }
-
         //Recheck if this number can actually be placed, 
         //because we may have updated the board by the time we get here
-        if(CanPlace(_board, x, y, viableNumberChecks[x][y][i]))
+        if(CanPlace(board, x, y, viableNumberChecks[x][y][i]))
         {
             //Try out this number
-            _board[x][y] = i;
+            board[x][y] = viableNumberChecks[x][y][i];
             
             //Do it again
-            if(BruteForceSudoku(_board))
+            if(BruteForceSudoku())
             {
                 //Returns true if we hit the base case of there being no more open places
                 return true;
             }
             
             //Failed to solve using the current number, so set it back to 0
-            _board[x][y] = EMPTY;
+            board[x][y] = EMPTY;
         }
     }
     //Backtrack to the previous function call
@@ -229,24 +240,19 @@ function GenerateRandomBoard()
     //Determine difficulty here
 
 
-
-
-
-
-
-
-
     /////////////////////////////////
     //Create and solve a board here//
     /////////////////////////////////
     console.log("Generating random Sudoku puzzle...");
-    var randomSequence = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let randomSequence = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     
     //Shuffle the sequence to make it random
     arrayShuffle(randomSequence);
     
     //Set the first row of the board to this random sequence
     Slice2DArray(emptyBoard, board);
+
+    //Set the top row to the new random sequence that was just generated
     board[0] = randomSequence.slice();
     console.log(board);
 
@@ -254,7 +260,8 @@ function GenerateRandomBoard()
     FindViableChecks(board);
 
     //Solve the board
-    if(!BruteForceSudoku(board))
+    //Start from the 1, 0 place since we randomly generated the first row
+    if(!BruteForceSudoku(0, 0))
     {
         //Can't solve the board???? this should be impossible!!!!
         console.log("Failed to solve the always solvable sudoku puzzle! You have a bug!");
@@ -276,13 +283,13 @@ function GenerateRandomBoard()
 //HTML, CSS, JAVASCRIPT, DOM stuff//
 ////////////////////////////////////
 
-//Web interface variables
-var selectedNum;
-var selectedTile;
-var disableSelect;
-var selectedBoardTilePos = 0;
+//Web interface letiables
+let selectedNum;
+let selectedTile;
+let disableSelect;
+let selectedBoardTilePos = 0;
 
-var difficulty = 0;
+let difficulty = 0;
 
 window.onload = function()
 {
@@ -383,12 +390,12 @@ function createGameBoard(board)
                         else
                         {
                             //Deselect the other tile
-                            var boardTiles = qsa(".tile");
+                            let boardTiles = qsa(".tile");
 
                             boardTiles[selectedBoardTilePos].classList.remove("selected");
                             foundSelected = true;
 
-                            //Add selection and update variable
+                            //Add selection and update letiable
                             tile.classList.add("selected");
                             selectedTile = tile;
                             selectedBoardTilePos = i * MBS + j;
@@ -426,8 +433,8 @@ function UpdateMove()
         selectedTile.textContent = selectedNum.textContent;
 
         //Divide the 1D array pos back to a 2D array pos
-        var row = Math.floor(selectedBoardTilePos / 9);
-        var column = selectedBoardTilePos % 9;
+        let row = Math.floor(selectedBoardTilePos / 9);
+        let column = selectedBoardTilePos % 9;
 
         //The the board position
         board[row][column] = parseInt(selectedNum.textContent, 10);
@@ -487,14 +494,14 @@ function id(id)
 //Utility functions
 
 //Function that creates multi dimentional arrays
-//Can be used like var arr = CreateArray(9, 9, 1);
+//Can be used like let arr = CreateArray(9, 9, 1);
 //Creates a 9x9 3D array with 1 element in the third dimension
 function CreateArray(length) {
-    var arr = new Array(length || 0),
+    let arr = new Array(length || 0),
         i = length;
 
     if (arguments.length > 1) {
-        var args = Array.prototype.slice.call(arguments, 1);
+        let args = Array.prototype.slice.call(arguments, 1);
         while(i--) arr[length-1 - i] = CreateArray.apply(this, args);
     }
 
@@ -508,7 +515,7 @@ function Slice2DArray(originalArr, copyArr)
     copyArr.splice(0, copyArr.length);
 
     //Add the contents of originalArr to copyArr by copy and not reference
-    for(var i = 0; i < originalArr.length; i++)
+    for(let i = 0; i < originalArr.length; i++)
     {
         copyArr.push(originalArr[i].slice());
     }
